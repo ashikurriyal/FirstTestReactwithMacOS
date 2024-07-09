d9149c4e2a1b54c2ff0520d239080e4c267ad43f
Ashikur Riyal <ashikurriyal@Ashikurs-MacBook-Pro.local>
2024-07-09 22:56:36 +0600

    Trying the new things for exploring

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ function App() {
 
   return (
     <div>
-      <h1>My First React App with MacOS</h1>
+      <h1 className='font-bold text-4xl'>My First React App with MacOS</h1>
+      <p>And this is running properly</p>
     </div>
   )
 }
